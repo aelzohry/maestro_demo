ee2319205612bf63966fee57fc81882a07f8e3ee
Ahmed Elzohry <aelzohry2010@gmail.com>
2023-02-21 06:10:12 +0200

    Initial commit

---
+++ MaestroDemo/MaestroDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaestroDemoApp.swift
+//  MaestroDemo
+//
+//  Created by Ahmed Elzohry on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MaestroDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
